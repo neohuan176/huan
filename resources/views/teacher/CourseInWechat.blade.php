ba7cd517f5ff418f9198f53183813d25df637b91
neohuan176 <a804258636@outlook.com>
2017-05-05 13:31:22 +0800

    微信开启点名修改。网页开启点名修改url。

--- resources/views/teacher/CourseInWechat.blade.php
+++ resources/views/teacher/CourseInWechat.blade.php
@@ -33,42 +33,6 @@
 
 
 
-
-    {{--<div class="row">--}}
-        {{--<div class="col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 main">--}}
-            {{--<table class="table table-striped">--}}
-                {{--<thead>--}}
-                {{--<tr>--}}
-                    {{--<th>名称</th>--}}
-                    {{--<th>编号</th>--}}
-                    {{--<th>地点</th>--}}
-                    {{--<th>时间</th>--}}
-                    {{--<th>点名</th>--}}
-                    {{--<th>操作</th>--}}
-                {{--</tr>--}}
-                {{--</thead>--}}
-                {{--<tbody>--}}
-                {{--@foreach($courses as $course)--}}
-                {{--<tr>--}}
-                    {{--<td>{{$course->Cname}}</td>--}}
-                    {{--<td>{{$course->Cno}}</td>--}}
-                    {{--<td>{{$course->Address}}</td>--}}
-                    {{--<td>{{$course->StartTime}} - {{$course->EndTime}}</td>--}}
-                    {{--<td>{{$course->weekday}}</td>--}}
-                    {{--<td class="isOpenCall @if($course->isOpenCall == 1) red @endif">@if($course->isOpenCall == 1)正在点名中...@else 未开启点名 @endif</td>--}}
-                    {{--<td>--}}
-                        {{--<button type="button" class="btn btn-primary" onclick="callOver(this)" id="{{$course->id}}">--}}
-                            {{--@if($course->isOpenCall == 1)关闭點名@else 开启点名 @endif--}}
-                        {{--</button>--}}
-                        {{--<button type="button" class="btn btn-danger" onclick="courseLocateInWechat({{$course->id}})">微信定位</button>--}}
-                    {{--</td>--}}
-                {{--</tr>--}}
-                    {{--@endforeach--}}
-                {{--</tbody>--}}
-            {{--</table>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-
     <script type="text/javascript">
         wx.config(<?php echo $js->config(array('getLocation'), false)?>);
         var addNewCallOver = 0;//是否开启新的一次点名；1:是 0:否
@@ -139,41 +103,6 @@
                 })
         }
 
-
-//        /**
-//         * 开启点名
-//         */
-//        function callOver(t){
-//            var courseId = $(t).attr('id');
-//            var course = null;//课程信息
-//            var addNewCallOver = 0;
-//            $.get("getCourse/"+courseId,
-//                function(data){
-//                    course = data;
-//
-//                    var now = Date.parse(new Date());
-//                    var lastCallOverTime = Date.parse(course.openCallOverTime);
-//                    console.log(new Date(),course.openCallOverTime);
-//                    if(now - lastCallOverTime > 270000 && !course.isOpenCall){//如果点名时间距离上一次点名时间大于45分钟就开启新的一次点名
-//                        //弹出弹框,先不谈，直接是新增一次点名
-//                        addNewCallOver = 1;
-//                        console.log("开启新的一次点名！");
-//                    }
-//                    $.get("changeCallOverStatus/"+courseId+"/"+addNewCallOver,
-//                        function(data){
-//                            if(data.status == 200){
-//                                var isOpenCall = data.isOpenCall?'<p style="color:#c9302c">正在点名中...</p>':'未开启';
-//                                var changOpenCallBtnText = data.isOpenCall?'关闭点名':'开启点名'
-//                                $(t).parent().parent().find('.isOpenCall').html(isOpenCall);
-//                                $(t).html(changOpenCallBtnText);
-//                            }
-//                            else{
-//                                console.log(data.errMsg);
-//                            }
-//                        })
-//
-//                });
-//        }
 
         function courseLocateInWechat(courseId){
             wx.ready(function() {
